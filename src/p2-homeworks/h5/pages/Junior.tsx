3c5175bb42dcfb62156628d4f873751acf17f0bf
GlennMiller1991 <gatesoftommorow91@mail.ru>
2021-10-24 23:54:57 +0300

    ninth homework was added

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 import styles from './Junior.module.css'
 import HW7 from '../../h7/HW7'
 import HW8 from "../../h8/HW8";
+import HW9 from "../../h9/HW9";
+import HW10 from "../../h10/HW10";
 
 const homeworks = [
     {
@@ -14,10 +16,20 @@ const homeworks = [
         code: <HW8/>,
         title: 'seventh homework'
     },
+    {
+        id: 2,
+        code: <HW9/>,
+        title: 'eighth homework'
+    },
+    {
+        id: 3,
+        code: <HW10/>,
+        title: 'ninth homework'
+    },
 ]
 
 function Junior() {
-    const [task, setTask] = useState<number>(1)
+    const [task, setTask] = useState<number>(homeworks.length - 1)
 
     return (
         <div className={styles.wrapper}>
